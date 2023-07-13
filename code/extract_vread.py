a7d607b1df286696e94dcbf17af052350a913663
Jake <jakewendt@gmail.com>
2023-07-13 13:42:25 -0700

    Corrected to process fasta not fastq. Trimmed leading > from read name. Used tabs.

--- code/extract_vread.py
+++ code/extract_vread.py
@@ -13,33 +13,38 @@ max_Eval = 1.0E-10
 blast_f = open(blast_res, "r")
 read_list = []
 for line in blast_f:
-    line = line.strip().split()
-    read = line[0]
-    Eval = line[10]
-    Eval = float(Eval)
+	line = line.strip().split()
+	read = line[0]
+	Eval = line[10]
+	Eval = float(Eval)
 
-    if Eval < max_Eval:
-        read_list.append(read)
+	if Eval < max_Eval:
+		read_list.append(read)
 
 read_set = set(read_list)
 
+#	This is clearly reading a FASTA (2 lines) file
 out = []
 fst = open(fasta, "r")
 fastl = []
 for line in fst:
-    line = line.strip()
-    fastl.append(line)
+	line = line.strip()
+	fastl.append(line)
 
+#	This is clearly expecting to have read a FASTQ (4 lines) file
+#for i in range(0,len(fastl),4): # clearly fastq
+#	read = fastl[i]
+#	seq = fastl[i + 1]
+#	plus = fastl[i + 2] # clearly fastq
+#	qual = fastl[i + 3] # clearly fastq
 
-for i in range(0,len(fastl),4):
-    read = fastl[i]
-    seq = fastl[i + 1]
-    plus = fastl[i + 2]
-    qual = fastl[i + 3]
-    if read in read_set:
-        read = '>' + read
-        l = [read,seq]
-        print("\n".join(l))
+for i in range(0,len(fastl),2):
+	read = fastl[i][1:] # remove leading ">" here
+	seq = fastl[i + 1]
+	if read in read_set:
+		read = '>' + read
+		l = [read,seq]
+		print("\n".join(l))
 
 
 
